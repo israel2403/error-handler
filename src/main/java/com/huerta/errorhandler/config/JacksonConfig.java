07747300ea83744f15891890eccad7afc98feb2a
Israel Huerta <israelhf24@gmail.com>
2024-04-23 20:29:30 -0600

    actualización

--- src/main/java/com/huerta/errorhandler/config/JacksonConfig.java
+++ src/main/java/com/huerta/errorhandler/config/JacksonConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class JacksonConfig {
@@ -13,7 +14,8 @@ public class JacksonConfig {
   public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
     Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
     builder.modules(
-      new SimpleModule().addSerializer(new CustomPagedModelSerializer())
+      new SimpleModule().addSerializer(new CustomPagedModelSerializer()),
+      new JavaTimeModule()
     );
     return builder;
   }
